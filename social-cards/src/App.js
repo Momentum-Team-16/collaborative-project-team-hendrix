2726bfc07711287c21e6108ceef8c730b7300185
Daniel <danielvillery@gmail.com>
2023-01-31 17:00:01 -0500

    initial main, must include your own token.json to use unsplash API, images are loading on page

--- social-cards/src/App.js
+++ social-cards/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import token from "./token.json";
 
+console.log(token.token);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <FrontCard token={token.token} />
     </div>
+  );
+}
+
+function FrontCard({ token }) {
+  const [img, setImg] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://api.unsplash.com/search/photos?query=mountains", {
+        headers: {
+          Authorization: `${token}`,
+        },
+      })
+      .then((res) => {
+        setImg(
+          res.data.results.map((obj) => ({
+            imgUrl: obj.urls.thumb,
+          }))
+        );
+      });
+  }, [token]);
+  return (
+    img.length > 0 && (
+      <div className='homepage'>
+        {img.map((i) => (
+          <img className='front-card' src={i.imgUrl} alt='work please' />
+        ))}
+      </div>
+    )
   );
 }
 
