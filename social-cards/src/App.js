9e071c5138c607947911ef3ba11a6ebaf57ee4ac
Marcus Nogueira <marcusnogueira23@gmail.com>
2023-02-02 11:22:33 -0500

    Troubleshooting -- Image on click during new post creation not appearing on Canvas

--- social-cards/src/App.js
+++ social-cards/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import token from "./token.json";
 import he from "he";
@@ -8,10 +8,11 @@ console.log(token.token);
 function App() {
   return (
     <div className='App'>
-      <SearchBar />
+      <NewPost />
     </div>
   );
 }
+
 // function Canvas({ canvasImg }) {
 //   drawImage(canvasImg);
 //   return (
@@ -20,6 +21,51 @@ function App() {
 //     </>
 //   );
 // }
+
+function NewPost (){
+  const [canvasImg, setCanvasImg] = useState(null);
+  return(
+    <div className="new-post">
+      <div className="buttons">
+        <FrontImageButton />
+        <AddTextButton />
+        <NextButton/>
+      </div>
+      <ImageCanvas canvasImg={canvasImg}/>
+      <SearchBar />
+      <FrontCard setCanvasImg={setCanvasImg}/>
+      
+    </div>
+  )
+}
+
+function ImageCanvas({canvasImg}){
+  // const canvasRef = useRef(null)
+  // const canvas = canvasRef.current
+  // const ctx = canvas.getContext('2d')
+
+  // ctx.drawImage( <img src={canvasImg} alt="pls work"/>,0,0)
+
+
+  // return <canvas ref={canvasRef} />
+  console.log(canvasImg)
+  return (
+    <canvas id="canvas"></canvas>
+  )
+}
+
+function AddTextButton(){
+
+}
+
+function FrontImageButton(){
+
+}
+
+function NextButton(){
+
+}
+
 function SearchBar() {
   const [text, setText] = useState("");
   const [query, setQuery] = useState("");
@@ -42,9 +88,10 @@ function SearchBar() {
     </div>
   );
 }
-function FrontCard({ token, query }) {
+
+function FrontCard({ token, query, setCanvasImg }) {
   const [img, setImg] = useState([]);
-  const [canvasImg, setCanvasImg] = useState(null);
+  
   useEffect(() => {
     axios
       .get(`https://api.unsplash.com/search/photos?query=${query}`, {
@@ -63,6 +110,7 @@ function FrontCard({ token, query }) {
   }, [token, query]);
 
   const handleClick = (i) => {
+    console.log("Clicked")
     setCanvasImg(i.imgUrl);
   };
 
