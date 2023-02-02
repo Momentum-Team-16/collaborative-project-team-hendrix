39b218df26134bce354c8e45ca0dd0de777cba9f
Daniel <danielvillery@gmail.com>
2023-02-01 22:03:57 -0500

    working search bar, need to add canvas to top of page and states to pick image

--- social-cards/src/App.js
+++ social-cards/src/App.js
@@ -12,6 +12,14 @@ function App() {
     </div>
   );
 }
+// function Canvas({ canvasImg }) {
+//   drawImage(canvasImg);
+//   return (
+//     <>
+//       <canvas className='canvas'></canvas>
+//     </>
+//   );
+// }
 function SearchBar() {
   const [text, setText] = useState("");
   const [query, setQuery] = useState("");
@@ -20,9 +28,10 @@ function SearchBar() {
     setQuery(text);
   };
   return (
-    <>
+    <div className='wrapper'>
       <form onSubmit={handleSubmit}>
         <input
+          className='search-bar'
           type='text'
           value={text}
           onChange={(e) => setText(e.target.value)}
@@ -30,16 +39,18 @@ function SearchBar() {
         <button>Search</button>
       </form>
       <FrontCard token={token.token} query={query} />
-    </>
+    </div>
   );
 }
 function FrontCard({ token, query }) {
   const [img, setImg] = useState([]);
+  const [canvasImg, setCanvasImg] = useState(null);
   useEffect(() => {
     axios
       .get(`https://api.unsplash.com/search/photos?query=${query}`, {
         headers: {
           Authorization: `${token}`,
+          "content-type": "application/x-www-form-urlencoded",
         },
       })
       .then((res) => {
@@ -50,11 +61,40 @@ function FrontCard({ token, query }) {
         );
       });
   }, [token, query]);
+
+  const handleClick = (i) => {
+    setCanvasImg(i.imgUrl);
+  };
+
+  // if (canvasImg != null) {
+  //   return (
+  //     <>
+  //       <Canvas canvasImg={canvasImg} />
+  //       <SearchBar />
+  //       <div className='homepage'>
+  //         {img.map((i) => (
+  //           <img
+  //             onClick={() => handleClick(i)}
+  //             className='front-card'
+  //             src={i.imgUrl}
+  //             alt='work please'
+  //           />
+  //         ))}
+  //       </div>
+  //     </>
+  //   );
+  // }
+
   return (
     img.length > 0 && (
       <div className='homepage'>
         {img.map((i) => (
-          <img className='front-card' src={i.imgUrl} alt='work please' />
+          <img
+            onClick={() => handleClick(i)}
+            className='front-card'
+            src={i.imgUrl}
+            alt='work please'
+          />
         ))}
       </div>
     )
