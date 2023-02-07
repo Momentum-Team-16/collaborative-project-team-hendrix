12133a3b23435669ec505f23add92912ea9e4e6d
CapelHoworth <capelhoworth@gmail.com>
2023-02-07 17:32:12 -0500

    routing logout/login. passing token

--- social-cards/src/App.js
+++ social-cards/src/App.js
@@ -1,65 +1,35 @@
 import "./App.css";
-import CreateCard from "./CreateCard";
 import React, { useEffect } from "react";
 import LogIn from "./LogIn";
 import { useState } from "react";
 import NewPost from "./CreateCard";
+import Homepage from "./Homepage";
 import axios from "axios";
+import { useNavigate, Link, Route, Routes } from "react-router-dom";
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [loginToken, setToken] = useState(null);
-  const [newPost, setNewPost] = useState(false);
-
-  if (newPost === true) {
-    if (loginToken === null) {
-      return <LogIn setToken={setToken} />;
-    }
-    return <NewPost setNewPost={setNewPost} loginToken={loginToken} />;
-  }
+  const [loginToken, setToken] = useLocalStorageState("SocialCardsToken", "");
 
   return (
-    <div className='App'>
-      <button onClick={() => setNewPost(true)}>New Post</button>
-      <Homepage loginToken={loginToken} />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage loginToken={loginToken} />}></Route>
+        <Route
+          path="/new/card"
+          element={<NewPost loginToken={loginToken} />}
+        ></Route>
+        <Route path="/login" element={<LogIn setToken={setToken} />}></Route>
+        <Route path="/logout" element={<LogOut setToken={setToken} />}></Route>
+      </Routes>
     </div>
   );
-  //
 }
 
-function Homepage({ loginToken }) {
-  const [cards, setCards] = useState(null);
-  useEffect(() => {
-    axios
-      .get("https://social-cards-wg2j.onrender.com/cards/")
-      .then((res) => setCards(res.data));
-  }, []);
-
-  console.log(cards);
-
-  return (
-    cards && (
-      <>
-        {cards.map((card) => (
-          <div className='canvas'>
-            <img
-              style={{
-                border: `5px ${card.border_style} ${card.border_color}`,
-              }}
-              className='canvas-img'
-              src={card.front_image}
-              alt=''
-            />
-            <div
-              className={card.text_align}
-              style={{ color: `${card.text_color}` }}
-            >
-              {card.front_message}
-            </div>
-          </div>
-        ))}
-      </>
-    )
-  );
+function LogOut({ setToken }) {
+  const navigate = useNavigate();
+  setToken(null);
+  navigate("/");
 }
 
 export default App;
--- social-cards/src/CreateCard.js
+++ social-cards/src/CreateCard.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import token from "./token.json";
 import placeholder from "./no-cover-image.png";
 import he from "he";
+import { Navigate, useNavigate, Link, Route, Routes } from "react-router-dom";
 
 // function Canvas({ canvasImg }) {
 //   drawImage(canvasImg);
@@ -16,16 +17,23 @@ import he from "he";
 
 function NewPost({ loginToken, setNewPost }) {
   const [canvasImg, setCanvasImg] = useState(placeholder);
-  const [frontText, setFrontText] = useState(null);
+  const [frontText, setFrontText] = useState("");
   const [frontTextColor, setFrontTextColor] = useState("black");
   const [textAlign, setTextAlign] = useState("center");
 
   const [bottomHalf, setBottomHalf] = useState("front-image");
   const [borderColor, setBorderColor] = useState("black");
   const [borderStyle, setBorderStyle] = useState("none");
+
+  if (!loginToken) {
+    return <Navigate to="/login" />;
+  }
   return (
-    <div className='new-post'>
-      <div className='navbar'>
+    <div className="new-post">
+      <div className="navbar">
+        <FrontImageButton setBottomHalf={setBottomHalf} />
+        <BorderButton setBottomHalf={setBottomHalf} />
+        <AddTextButton setBottomHalf={setBottomHalf} />
         <SaveButton
           loginToken={loginToken}
           canvasImg={canvasImg}
@@ -36,9 +44,6 @@ function NewPost({ loginToken, setNewPost }) {
           borderStyle={borderStyle}
           setNewPost={setNewPost}
         />
-        <FrontImageButton setBottomHalf={setBottomHalf} />
-        <BorderButton setBottomHalf={setBottomHalf} />
-        <AddTextButton setBottomHalf={setBottomHalf} />
       </div>
       <ImageCanvas
         canvasImg={canvasImg}
@@ -67,13 +72,19 @@ function NewPost({ loginToken, setNewPost }) {
 
 // NAVBAR BUTTONS
 const FrontImageButton = ({ setBottomHalf }) => (
-  <button onClick={() => setBottomHalf("front-image")}>Front</button>
+  <button className="effect" onClick={() => setBottomHalf("front-image")}>
+    Front
+  </button>
 );
 const AddTextButton = ({ setBottomHalf }) => (
-  <button onClick={() => setBottomHalf("add-text")}>Text</button>
+  <button className="effect" onClick={() => setBottomHalf("add-text")}>
+    Text
+  </button>
 );
 const BorderButton = ({ setBottomHalf }) => (
-  <button onClick={() => setBottomHalf("border-select")}>Border</button>
+  <button className="effect" onClick={() => setBottomHalf("border-select")}>
+    Border
+  </button>
 );
 
 function SaveButton({
@@ -86,6 +97,7 @@ function SaveButton({
   borderStyle,
   setNewPost,
 }) {
+  const navigate = useNavigate();
   const handleClick = (e) => {
     console.log(canvasImg);
     console.log(frontText, frontTextColor, borderColor);
@@ -106,9 +118,13 @@ function SaveButton({
         },
       }
     );
-    setNewPost(false);
+    navigate("/");
   };
-  return <button onClick={handleClick}>Save</button>;
+  return (
+    <button className="effect" onClick={handleClick}>
+      Save
+    </button>
+  );
 }
 
 //RENDERS THE BOTTOM HALF OF THE NEW POST PAGE
@@ -143,8 +159,8 @@ function BottomHalf(props) {
 function ImageCanvas(props) {
   if (props.borderStyle === "none") {
     return (
-      <div className='canvas'>
-        <img className='canvas-img' src={props.canvasImg} alt='' />
+      <div className="canvas">
+        <img className="canvas-img" src={props.canvasImg} alt="" />
         <div
           className={props.textAlign}
           style={{ color: `${props.frontTextColor}` }}
@@ -155,12 +171,12 @@ function ImageCanvas(props) {
     );
   } else {
     return (
-      <div className='canvas'>
+      <div className="canvas">
         <img
           style={{ border: `5px ${props.borderStyle} ${props.borderColor}` }}
-          className='canvas-img'
+          className="canvas-img"
           src={props.canvasImg}
-          alt=''
+          alt=""
         />
         <div
           className={props.textAlign}
@@ -183,16 +199,16 @@ function SearchBar({ setCanvasImg }) {
   };
   return (
     <>
-      <form className='form' onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <input
-          className='search-bar'
-          type='text'
+          className="search-bar"
+          type="text"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
         <button>Search</button>
       </form>
-      <div className='wrapper'>
+      <div className="wrapper">
         <FrontCard
           token={token.token}
           query={query}
@@ -230,16 +246,16 @@ function FrontCard({ token, query, setCanvasImg }) {
 
   return (
     img.length > 0 && (
-      <div className='return-box'>
-        <ul key={query} className='search-returns'>
+      <div className="return-box">
+        <ul key={query} className="search-returns">
           {img.map((i) => (
-            <div className='search-wrapper'>
+            <div className="search-wrapper">
               <img
                 key={i.imgUrl}
-                className='search-element'
+                className="search-element"
                 onClick={() => handleClick(i)}
                 src={i.imgUrl}
-                alt='search result'
+                alt="search result"
               />
             </div>
           ))}
@@ -284,32 +300,32 @@ function TextInput(props) {
     props.setTextAlign(e.target.value);
   };
   return (
-    <div className='text-customizer'>
-      <div className='front-input'>
+    <div className="text-customizer">
+      <div className="front-input">
         <input onChange={handleText} value={textInputField} />
       </div>
-      <label htmlFor='text-color'>
+      <label htmlFor="text-color">
         <select
           value={props.frontTextColor}
           onChange={(e) => props.setFrontTextColor(e.target.value)}
         >
-          <option value='black'>Black</option>
-          <option value-='red'>Red</option>
-          <option value='green'>Green</option>
-          <option value='purple'>Purple</option>
-          <option value='blue'>Blue</option>
-          <option value='yellow'>Yellow</option>
-          <option value='orange'>Orange</option>
-          <option value='pink'>Pink</option>
-          <option value='white'>White</option>
+          <option value="black">Black</option>
+          <option value-="red">Red</option>
+          <option value="green">Green</option>
+          <option value="purple">Purple</option>
+          <option value="blue">Blue</option>
+          <option value="yellow">Yellow</option>
+          <option value="orange">Orange</option>
+          <option value="pink">Pink</option>
+          <option value="white">White</option>
         </select>
       </label>
 
-      <label htmlFor='text-alignment'>
+      <label htmlFor="text-alignment">
         <select value={displayAlign} onChange={handleAlignment}>
-          <option value='center'>Center</option>
-          <option value='top'>Top</option>
-          <option value='bottom'>Bottom</option>
+          <option value="center">Center</option>
+          <option value="top">Top</option>
+          <option value="bottom">Bottom</option>
         </select>
       </label>
     </div>
@@ -319,36 +335,36 @@ function TextInput(props) {
 //Customize Border Color and Style
 function BorderSelect(props) {
   return (
-    <div className='border-select'>
+    <div className="border-select">
       <>
-        <label htmlFor='border-style'>
+        <label htmlFor="border-style">
           Pick a Border Style
           <select
             value={props.borderStyle}
             onChange={(e) => props.setBorderStyle(e.target.value)}
           >
-            <option value='none'>None</option>
-            <option value='solid'>Solid</option>
-            <option value-='dotted'>Dotted</option>
-            <option value='double'>Double</option>
+            <option value="none">None</option>
+            <option value="solid">Solid</option>
+            <option value-="dotted">Dotted</option>
+            <option value="double">Double</option>
           </select>
         </label>
 
-        <label htmlFor='border-color'>
+        <label htmlFor="border-color">
           Pick a Border Color
           <select
             value={props.borderColor}
             onChange={(e) => props.setBorderColor(e.target.value)}
           >
-            <option value='black'>Black</option>
-            <option value-='red'>Red</option>
-            <option value='green'>Green</option>
-            <option value='purple'>Purple</option>
-            <option value='blue'>Blue</option>
-            <option value='yellow'>Yellow</option>
-            <option value='orange'>Orange</option>
-            <option value='pink'>Pink</option>
-            <option value='white'>White</option>
+            <option value="black">Black</option>
+            <option value-="red">Red</option>
+            <option value="green">Green</option>
+            <option value="purple">Purple</option>
+            <option value="blue">Blue</option>
+            <option value="yellow">Yellow</option>
+            <option value="orange">Orange</option>
+            <option value="pink">Pink</option>
+            <option value="white">White</option>
           </select>
         </label>
       </>
